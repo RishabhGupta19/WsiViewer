016be53eb6d0901d2707fcc791614df9459f7002
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-04-06 00:16:28 +0530

    Update LeftPanel.jsx

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -1,3 +1,21 @@
+// import React from "react";
+
+// const LeftPanel = ({ patientId, sampleType, date, detectionCount }) => {
+//   return (
+//     <div className="bg-gray-100 p-4 h-full overflow-auto">
+//       <h2 className="text-xl font-bold mb-4">Patient Details</h2>
+//       <p><strong>Patient ID:</strong> {patientId}</p>
+//       <p><strong>Sample Type:</strong> {sampleType}</p>
+//       <p><strong>Date:</strong> {date}</p>
+//       <p><strong>Findings:</strong> {detectionCount} Circular RBCs detected</p>
+//       <h2 className="text-xl font-bold mb-4">Slide View</h2>
+      
+//     </div>
+//   );
+// };
+
+// export default LeftPanel;
+
 import React from "react";
 
 const LeftPanel = ({ patientId, sampleType, date, detectionCount }) => {
@@ -9,7 +27,6 @@ const LeftPanel = ({ patientId, sampleType, date, detectionCount }) => {
       <p><strong>Date:</strong> {date}</p>
       <p><strong>Findings:</strong> {detectionCount} Circular RBCs detected</p>
       <h2 className="text-xl font-bold mb-4">Slide View</h2>
-      
     </div>
   );
 };
