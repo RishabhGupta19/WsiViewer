b505c548f1799e77c075ab86a3dbc3e11c795625
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-04-05 22:32:33 +0530

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -8,19 +8,32 @@
 //     },
 //   };
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
-    },
-  },
   webpack: {
     configure: (webpackConfig, { env }) => {
       if (env === 'production') {
+        // Remove React Refresh plugin
         webpackConfig.plugins = webpackConfig.plugins.filter(
           plugin => plugin.constructor.name !== 'ReactRefreshPlugin'
         );
+        // Ensure no refresh runtime is included
+        webpackConfig.module.rules.forEach(rule => {
+          if (rule.oneOf) {
+            rule.oneOf.forEach(oneOfRule => {
+              if (oneOfRule.use && oneOfRule.use.loader === 'babel-loader') {
+                oneOfRule.use.options.plugins = oneOfRule.use.options.plugins?.filter(
+                  plugin => !plugin?.includes('react-refresh')
+                );
+              }
+            });
+          }
+        });
       }
       return webpackConfig;
+    },
+  },
+  style: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
 };
