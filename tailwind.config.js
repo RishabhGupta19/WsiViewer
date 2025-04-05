f495716e6ee3da4bcf71e5fa032af24b6440406f
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-04-05 22:23:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       width: {
+//         '3/10': '30%',
+//         '7/10': '70%',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      width: {
-        '3/10': '30%',
-        '7/10': '70%',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
